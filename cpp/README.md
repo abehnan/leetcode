8fc049f6755c87b10b7b7f22e706c334f28b3c01
abehnan <abehnan@gmail.com>
2023-03-16 20:13:01 -0400

    Completed merge_two_lists.cpp

---
+++ cpp/README.md
@@ -0,0 +1,10 @@
+# leetcode
+
+This project is where I store solution of some Leetcode problems I've worked on.
+
+#### Disclaimer on C++ Memory Management
+
+Leetcode's tests often expect raw pointers to objects allocated on the heap.
+
+That said, in a real world scenario, the use of smart pointers such as std::unique_ptr and std::shared_ptr should be
+preferred to the new/delete operators in order to avoid memory leaks.
--- cpp/merge_two_lists.cpp
+++ cpp/merge_two_lists.cpp
@@ -10,20 +10,47 @@
 using namespace ListHelper;
 
 class Solution {
-//public:
-//    static ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
-//        return nullptr;
-//    }
+public:
+    static ListNode *mergeTwoLists(ListNode *left, ListNode *right) {
+        auto result = new ListNode();
+        auto ans = result;
+
+        while (left || right) {
+            if (left && right) {
+                if (left->val < right->val) {
+                    result->next = new ListNode(left->val);
+                    left = left->next;
+                } else {
+                    result->next = new ListNode(right->val);
+                    right = right->next;
+                }
+            } else if (left) {
+                result->next = new ListNode(left->val);
+                left = left->next;
+            } else {
+                result->next = new ListNode(right->val);
+                right = right->next;
+            }
+
+            result = result->next;
+        }
+
+        return ans->next;
+    }
 };
 
 int main() {
-    ListNode *firstNumber = buildList(457);
+    ListNode *firstNumber = buildList(754);
     int x = getNumber(firstNumber);
-    assert(x == 457);
+    assert(x == 754);
 
-    ListNode *secondNumber = buildList(129);
+    ListNode *secondNumber = buildList(921);
     int y = getNumber(secondNumber);
-    assert(y == 129);
+    assert(y == 921);
+
+    auto list = Solution::mergeTwoLists(firstNumber, secondNumber);
+    auto number = getNumber(list);
+    assert(number == 975421); // note that getNumber assumes the list stores a number in reverse order
 
     return 0;
 }
