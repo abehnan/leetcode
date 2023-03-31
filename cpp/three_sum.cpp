bd77ce5a3b2eaeb18083e6d1996661c551b03a60
abehnan <abehnan@gmail.com>
2023-03-31 11:32:54 -0400

    Added three_sum.cpp

---
+++ cpp/three_sum.cpp
@@ -0,0 +1,32 @@
+/**
+ * Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+ * Notice that the solution set must not contain duplicate triplets.
+ *
+ * Input: nums = [-1,0,1,2,-1,-4]
+ * Output: [[-1,-1,2],[-1,0,1]]
+ * Explanation:
+ * nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+ * nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+ * nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+ * The distinct triplets are [-1,0,1] and [-1,-1,2].
+ * Notice that the order of the output and the order of the triplets does not matter.
+ */
+
+#include <cassert>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	static vector<vector<int>> threeSum(const vector<int>& /*nums*/)
+	{
+		return {};
+	}
+};
+
+int main()
+{
+	assert(Solution::threeSum({ 0, 1, 1 }).empty());
+	return 0;
+}
