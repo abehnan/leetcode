b8878c7cf5a0aafd1549ccee394b5cffe73da87e
abehnan <abehnan@gmail.com>
2023-03-31 14:47:12 -0400

    Improved solution for container_with_most_water.cpp

--- cpp/container_with_most_water.cpp
+++ cpp/container_with_most_water.cpp
@@ -3,8 +3,14 @@
  * Find two lines that together with the x-axis form a container, such that the container contains the most water.
  * Return the maximum amount of water a container can store.
  * Notice that you may not slant the container.
+ *
+ * Constraints:
+ *	n == height.length
+ *	2 <= n <= 105
+ *	0 <= height[i] <= 104
  */
 
+#include <algorithm>
 #include <cassert>
 #include <vector>
 
@@ -12,15 +18,66 @@ using namespace std;
 
 class Solution {
 public:
-	static int maxArea(vector<int>& /*height*/)
+	// Brute Force, O(n^2)
+	static int maxAreaBruteForce(const vector<int>& height)
 	{
-		return 0;
+		int n = static_cast<int>(height.size());
+		int result = 0;
+
+		for (int l = 0; l < n; ++l) {
+			for (int r = n - 1; r > l; --r) {
+				int area = min(height[l], height[r]) * (r - l);
+				result = max(result, area);
+			}
+		}
+
+		return result;
+	}
+
+	// Solution turns into O(n) if you use a sliding window instead.
+	static int maxArea(const vector<int>& height)
+	{
+		int n = static_cast<int>(height.size());
+		int l = 0, r = n - 1;
+		int maxArea = 0;
+
+		while (l < r) {
+			int area = min(height[l], height[r]) * (r - l);
+			maxArea = max(maxArea, area);
+
+			if (height[l] < height[r]) {
+				l++;
+			} else {
+				r--;
+			}
+		}
+
+		return maxArea;
 	}
 };
 
 int main()
 {
 	vector<int> input;
+	int output, expected;
+
 	input = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
-	assert(Solution::maxArea(input) == 0);
+	output = Solution::maxAreaBruteForce(input);
+	expected = 49;
+	assert(output == expected);
+
+	input = { 1, 1 };
+	output = Solution::maxAreaBruteForce(input);
+	expected = 1;
+	assert(output == expected);
+
+	input = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+	output = Solution::maxArea(input);
+	expected = 49;
+	assert(output == expected);
+
+	input = { 1, 1 };
+	output = Solution::maxArea(input);
+	expected = 1;
+	assert(output == expected);
 }
