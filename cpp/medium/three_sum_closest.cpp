03ee24140deb5b3ddc56e9c018af664c95222116
abehnan <abehnan@gmail.com>
2023-03-31 15:44:55 -0400

    Added description to three_sum_closest.cpp

--- cpp/medium/three_sum_closest.cpp
+++ cpp/medium/three_sum_closest.cpp
@@ -1,15 +1,7 @@
 /**
- * Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
- * Notice that the solution set must not contain duplicate triplets.
- *
- * Input: nums = [-1,0,1,2,-1,-4]
- * Output: [[-1,-1,2],[-1,0,1]]
- * Explanation:
- * nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
- * nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
- * nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
- * The distinct triplets are [-1,0,1] and [-1,-1,2].
- * Notice that the order of the output and the order of the triplets does not matter.
+ * Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+ * Return the sum of the three integers.
+ * You may assume that each input would have exactly one solution.
  */
 
 #include <algorithm>
