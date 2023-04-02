3127d527292b4ce7177099cac38a8962860c3a4c
abehnan <abehnan@gmail.com>
2023-04-01 20:50:19 -0400

    Added contains_duplicate.cpp

---
+++ cpp/easy/contains_duplicate.cpp
@@ -0,0 +1,87 @@
+/**
+ * Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+ * Constraints:
+ * 		1 <= nums.length <= 105
+ * 		-109 <= nums[i] <= 109
+ */
+
+#include <algorithm>
+#include <cassert>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	static bool containsDuplicateNoExtraSpace(vector<int>& nums)
+	{
+		if (nums.size() <= 1) {
+			return false;
+		}
+
+		sort(nums.begin(), nums.end());
+		auto prev = INT_MIN;
+
+		for (const auto& num : nums) {
+			if (num == prev) {
+				return true;
+			}
+
+			prev = num;
+		}
+
+		return false;
+	}
+
+	static bool containsDuplicate(const vector<int>& nums)
+	{
+		auto s = set<int>(nums.begin(), nums.end());
+		return s.size() != nums.size();
+	}
+};
+
+int main()
+{
+	auto input = std::vector<int> {};
+	auto expected = false;
+	auto actual = Solution::containsDuplicateNoExtraSpace(input);
+	assert(actual == expected);
+
+	input = { 1, 2, 3, 1 };
+	expected = true;
+	actual = Solution::containsDuplicateNoExtraSpace(input);
+	assert(actual == expected);
+
+	input = { 1, 2, 3, 4 };
+	expected = false;
+	actual = Solution::containsDuplicateNoExtraSpace(input);
+	assert(actual == expected);
+
+	input = { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
+	expected = true;
+	actual = Solution::containsDuplicateNoExtraSpace(input);
+	assert(actual == expected);
+
+	input = {};
+	expected = false;
+	actual = Solution::containsDuplicate(input);
+	assert(actual == expected);
+
+	input = { 1, 2, 3, 1 };
+	expected = true;
+	actual = Solution::containsDuplicate(input);
+	assert(actual == expected);
+
+	input = { 1, 2, 3, 4 };
+	expected = false;
+	actual = Solution::containsDuplicate(input);
+	assert(actual == expected);
+
+	input = { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
+	expected = true;
+	actual = Solution::containsDuplicate(input);
+	assert(actual == expected);
+
+	return 0;
+}
