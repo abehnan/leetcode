3855975a1de78b6704f55b5c90dc53708e3eaa66
abehnan <abehnan@gmail.com>
2023-07-01 17:46:34 -0400

    Added group_anagrams.cpp and valid_anagram.cpp

---
+++ cpp/easy/valid_anagram.cpp
@@ -0,0 +1,31 @@
+/**
+ * Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+ * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+ * Constraints:
+ *  - 1 <= s.length, t.length <= 5 * 104
+ *  - s and t consist of lowercase English letters.
+ */
+
+#include <algorithm>
+#include <cassert>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+	// O(n log n)
+	static int validAnagram(string s, string t)
+	{
+		sort(s.begin(), s.end());
+		sort(t.begin(), t.end());
+		return s == t;
+	}
+};
+
+int main()
+{
+	assert(Solution::validAnagram("anagram", "nagaram"));
+	assert(!Solution::validAnagram("rat", "car"));
+	return 0;
+}
---
+++ cpp/medium/group_anagrams.cpp
@@ -0,0 +1,49 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "performance-inefficient-vector-operation"
+/**
+ * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+ * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+ * Constraints:
+ *  - 1 <= strs.length <= 104
+ *  - 0 <= strs[i].length <= 100
+ *  - strs[i] consists of lowercase English letters.
+ */
+
+#include <algorithm>
+#include <cassert>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+	static vector<vector<string>> groupAnagrams(vector<string>& strs)
+	{
+		unordered_map<string, vector<string>> anagrams;
+
+		for (const auto& str : strs) {
+			auto key = str;
+			sort(key.begin(), key.end());
+			anagrams[key].emplace_back(str);
+		}
+
+		vector<vector<string>> result;
+
+		for (const auto& [k, v] : anagrams) {
+			result.emplace_back(v);
+		}
+
+		return result;
+	}
+};
+
+int main()
+{
+	auto input = vector<string> { "eat", "tea", "tan", "ate", "nat", "bat" };
+	auto expected = vector<vector<string>> { { "bat" }, { "nat", "tan" }, { "ate", "eat", "tea" } };
+	auto result = Solution::groupAnagrams(input);
+	assert(result == expected);
+	return 0;
+}
+
+#pragma clang diagnostic pop
