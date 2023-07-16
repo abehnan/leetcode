3825cca09d680e6946bac63aaae89e6238cec346
abehnan <abehnan@gmail.com>
2023-07-16 18:14:46 -0400

    Added solution to trapping_rain_water.cpp

--- cpp/hard/trapping_rain_water.cpp
+++ cpp/hard/trapping_rain_water.cpp
@@ -15,9 +15,27 @@ using namespace std;
 
 class Solution {
 public:
-	static int trap(vector<int>& /*height*/)
+	static int trap(const vector<int>& height)
 	{
-		return 0;
+		auto l = 0u;
+		auto r = height.size() - 1;
+		auto maxLeft = height[l];
+		auto maxRight = height[r];
+		auto result = 0;
+
+		while (l < r) {
+			if (maxLeft <= maxRight) {
+				l++;
+				maxLeft = max(maxLeft, height[l]);
+				result += maxLeft - height[l];
+			} else {
+				r--;
+				maxRight = max(maxRight, height[r]);
+				result += maxRight - height[r];
+			}
+		}
+
+		return result;
 	}
 };
 
@@ -26,5 +44,9 @@ int main()
 	auto input = vector<int> { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
 	auto output = Solution::trap(input);
 	auto expected = 6;
+	assert(output == expected);
+	input = vector<int> { 4, 2, 0, 3, 2, 5 };
+	output = Solution::trap(input);
+	expected = 9;
 	assert(output == expected);
 }
