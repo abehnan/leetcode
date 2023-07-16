cdc6bf9bd3ef57321313846a372b664ed5fa2add
abehnan <abehnan@gmail.com>
2023-07-16 18:58:50 -0400

    Added easy solution to trapping_rain_water.cpp

--- cpp/hard/trapping_rain_water.cpp
+++ cpp/hard/trapping_rain_water.cpp
@@ -15,7 +15,8 @@ using namespace std;
 
 class Solution {
 public:
-	static int trap(const vector<int>& height)
+	// O(n) with no extra space.
+	static int trapOptimized(const vector<int>& height)
 	{
 		auto l = 0u;
 		auto r = height.size() - 1;
@@ -37,14 +38,65 @@ public:
 
 		return result;
 	}
+
+	// O(n) time and O(n) space, but easier.
+	static int trap(const vector<int>& height)
+	{
+		auto n = height.size();
+
+		if (n < 3) {
+			return 0;
+		}
+
+		auto result = 0;
+		auto maxLeft = vector<int>(n, 0);
+		auto maxRight = vector<int>(n, 0);
+		auto minBoundary = vector<int>(n, 0);
+		auto currentMax = height[0];
+
+		for (auto i = 1u; i < n; i++) {
+			currentMax = max(height[i - 1], currentMax);
+			maxLeft[i] = currentMax;
+		}
+
+		currentMax = height[n - 1];
+
+		for (auto i = static_cast<int>(n - 2); i >= 0; i--) {
+			currentMax = max(height[i + 1], currentMax);
+			maxRight[i] = currentMax;
+		}
+
+		for (auto i = 0u; i < n; i++) {
+			minBoundary[i] = min(maxLeft[i], maxRight[i]);
+		}
+
+		for (auto i = 0u; i < n; i++) {
+			result += max(minBoundary[i] - height[i], 0);
+		}
+
+		return result;
+	}
 };
 
 int main()
 {
 	auto input = vector<int> { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
-	auto output = Solution::trap(input);
+	auto output = Solution::trapOptimized(input);
 	auto expected = 6;
 	assert(output == expected);
+
+	Solution::trap(input);
+	input = vector<int> { 4, 2, 0, 3, 2, 5 };
+	output = Solution::trapOptimized(input);
+	expected = 9;
+	assert(output == expected);
+
+	input = vector<int> { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+	output = Solution::trap(input);
+	expected = 6;
+	assert(output == expected);
+
+	Solution::trap(input);
 	input = vector<int> { 4, 2, 0, 3, 2, 5 };
 	output = Solution::trap(input);
 	expected = 9;
