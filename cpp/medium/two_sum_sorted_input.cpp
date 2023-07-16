47d86a43fdf1f95a447e978dbc94f86c42f8c2f4
abehnan <abehnan@gmail.com>
2023-07-16 17:10:40 -0400

    Added tests to two_sum_sorted_input.cpp

--- cpp/medium/two_sum_sorted_input.cpp
+++ cpp/medium/two_sum_sorted_input.cpp
@@ -1,11 +1,11 @@
 /**
- *Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number.
- *Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 < numbers.length.
- *Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
- *The tests are generated such that there is exactly one solution. You may not use the same element twice.
- *Your solution must use only constant extra space.
+ * Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number.
+ * Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 < numbers.length.
+ * Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+ * The tests are generated such that there is exactly one solution. You may not use the same element twice.
+ * Your solution must use only constant extra space.
  *
- *Constraints:
+ * Constraints:
  *	- 2 <= numbers.length <= 3 * 104
  *	- -1000 <= numbers[i] <= 1000
  *	- numbers is sorted in non-decreasing order.
@@ -28,7 +28,16 @@ public:
 
 int main()
 {
-	auto input = vector<int> {};
-	assert(Solution::twoSum(input, 0).empty());
+	auto input = vector<int> { 2, 7, 11, 15 };
+	auto expected = vector<int> { 1, 2 };
+	assert(Solution::twoSum(input, 9) == expected);
+
+	input = vector<int> { 2, 3, 4 };
+	expected = vector<int> { 1, 3 };
+	assert(Solution::twoSum(input, 6) == expected);
+
+	input = vector<int> { -1, 0 };
+	expected = vector<int> { 1, 2 };
+	assert(Solution::twoSum(input, -1) == expected);
 	return 0;
 }
