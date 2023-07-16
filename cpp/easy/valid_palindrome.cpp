91bacc108cd85b7f62d13bfb7d00a4e792e1d516
abehnan <abehnan@gmail.com>
2023-07-16 16:10:33 -0400

    Added constraints to valid_palindrome.cpp

--- cpp/easy/valid_palindrome.cpp
+++ cpp/easy/valid_palindrome.cpp
@@ -2,6 +2,10 @@
  * A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward.
  * Alphanumeric characters include letters and numbers.
  * Given a string s, return true if it is a palindrome, or false otherwise.s.
+ *
+ * Constraints:
+ * 	- 1 <= s.length <= 2 * 105
+ * 	- s consists only of printable ASCII characters.
  */
 
 #include <cassert>
